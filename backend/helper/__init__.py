dac4f20f10257879aa6e48a872607c70b43f3f28
Jamie Ogundiran <jamieogundiran@gmail.com>
2025-04-09 15:26:00 +0100

    Cleaner code

--- backend/helper/__init__.py
+++ backend/helper/__init__.py
@@ -1,2 +1,2 @@
 from .ig_helper import load_access_token, send_instagram_message, reply_to_instagram_comment
-from .fb_helper import send_facebook_message, get_facebook_posts 
+from .fb_helper import FacebookApiClient
--- backend/helper/ig_helper.py
+++ backend/helper/ig_helper.py
@@ -1,5 +1,9 @@
 import json
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 IG_TOKEN_PATH = "ig_token.json"
 INSTAGRAM_API_URL = "https://graph.instagram.com/v21.0/me/messages"
@@ -11,11 +15,11 @@ def load_access_token(path=IG_TOKEN_PATH):
     with open(path, "r") as f:
         return json.load(f)["access_token"]
 
-
 def send_instagram_message(user_access_token, recipient_id, message_text):
     """
     Send a text message to an Instagram user via the Messaging API.
     """
+
     headers = {
         "Authorization": f"Bearer {user_access_token}",
         "Content-Type": "application/json"
@@ -35,15 +39,15 @@ def reply_to_instagram_comment(comment_id, message_text, access_token=None):
     """
     Reply to an Instagram comment using the Facebook Graph API.
     """
-    if not access_token:
-        access_token = load_access_token()
+    
+    access_token = os.getenv("FACEBOOK_ACCESS_TOKEN")
         
     url = f"https://graph.facebook.com/v21.0/{comment_id}/replies"
     print(comment_id)
     
     payload = {
         "message": message_text,
-        "access_token":'EAAQp5L2eW58BO1D8EtbXBEuImzUhCmk8iaiRdVTrmCk7YKYRl6CuilZBFzqpPyRcgvcbB13AHb8idOxyZAHb8Kmzw5WaEZB6W1aauNjCVkccIHMBI6wWCdIkXZB6WByxUbg1r3e9KytmZCkDSS8OvtSKEk00F7nHjn9GmK7lO5a2Y6TyfLBz1DIJWOX3L0ZBD7n09Kq5n7zHPxtiAMpLaOlu96VjEKl2CZARZAYZD'
+        "access_token":access_token
     }
     
     # Notice we're using params instead of data here
